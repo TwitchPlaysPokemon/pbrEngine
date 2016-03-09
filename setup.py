83bdd7a5aad01e8bbc8badae4df8cbc4c905e8c0
Felk <studio.felk@googlemail.com>
2016-03-09 15:26:50 +0100

    remove not working dependency_links.

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     name="pbrEngine",
     version="0.1",
     packages=["pbrEngine"],
-    setup_requires=['dolphinWatch'],
     install_requires=['dolphinWatch'],
-    dependency_links=['https://github.com/TwitchPlaysPokemon/PyDolphinWatch/tarball/master#egg=dolphinWatch'],
 
     author="Felk",
     description="Library based on DolphinWatch to offer automation of Pokemon Battle Revolution matches for TwitchPlaysPokemon.",
