33054ca988b87e0209f7e48b6b0a014150770a59
Felk <studio.felk@googlemail.com>
2016-03-12 12:32:47 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name="pbrEngine",
-    version="0.2",
+    version="0.3-dev",
     packages=find_packages(),
     install_requires=['dolphinWatch'],
 
