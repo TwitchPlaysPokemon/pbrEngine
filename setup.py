dbbe6346ff386203f6116d8f96df6c980a3341fa
Felk <studio.felk@googlemail.com>
2016-04-29 18:28:15 +0200

    bumped version to 0.5.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name="pbrengine",
-    version="0.5.8-dev",
+    version="0.5.8",
     packages=find_packages(),
     install_requires=['dolphinWatch'],
 
